061d4bafde714204f1d0867487261c3b73a77e9d
ILViTs <tsoyilya@gmail.com>
2023-11-11 22:15:37 +0300

    init

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,3 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,22 +5,39 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteLoaderData,
 } from "@remix-run/react";
+import {
+  json,
+  type LinksFunction,
+  type LoaderFunctionArgs,
+} from "@remix-run/node";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import MainNavigation from "./components/MainNavigation";
+import { authenticator } from "./auth/authenticator.server";
+import styles from "./styles/tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return json(await authenticator.isAuthenticated(request));
+};
+
+export function useRootLoaderData() {
+  return useRouteLoaderData<typeof loader>("root");
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
+        <MainNavigation />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
+import { type MetaFunction } from "@remix-run/node";
+
+// import { authenticator } from "~/auth/authenticator.server";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "500 Eskimo" },
+    { name: "description", content: "Welcome to 500 Eskimo portal!" },
   ];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <main className='p-4 h-screen'>INDEX</main>;
 }
